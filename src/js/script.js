2ea86282c525b4642bcf48e6b312a74f8aeed352
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-01-05 17:36:17 +0200

    homework08

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,54 @@
 console.log('script launched');
 let detailsImage = document.querySelector('.details-image');
 let detailsTitle = document.querySelector('.details-title');
+let mainContentEl = document.querySelector('.main-content');
+let selectedItem;
+let deleteBorder;
 
-let anchors = document.querySelectorAll('.thumbnails-anchor'); //all HTML elements belonging to the class thumbnails-anchor
-for (let i = 0; i < anchors.length; i++) {
+let anchors = document.querySelectorAll('.thumbnails-anchor');
+for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener('click', function(event) {
-        event.preventDefault(); //canceled default behavior of aanchor elemet hitting
-        setDetails(anchors[i]); //setDetails function call with passing regerence to appropriate anchor
-    })
+        event.preventDefault(); 
+        showDetails();
+        setDetails(anchors[i]); 
+       
+    });
 }
 
 function setDetails(anchor) {
     console.log('anchor element was pressed', anchor);
-    detailsImage.setAttribute('src', anchor.getAttribute('href'));
-    detailsTitle.textContent = anchor.getAttribute('data-details-title');
+    let hrefValue = anchor.getAttribute('href');
+    detailsImage.setAttribute('src', hrefValue);
+
+    anchor.parentElement.classList.add('selected');
+    if (selectedItem) {
+        selectedItem.classList.remove('selected');
+    }
+    selectedItem = anchor.parentElement;
+
+    if (selectedItem){
+        selectedItem.classList.remove("selected");
+    }
+
+
+    
+
+   
+
+    //get element with class thumbnails-title inside the given anchor
+    let  thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
+    let thumbnailsTitleElement = document.querySelector(thumbnailsTitleSelector);
+    //dog name exist inside thumbnailTitleElenent.textContent
+    detailsTitle.textContent = `${thumbnailsTitleElement.textContent}: ${anchor.getAttribute('data-details-title')}`;
 }
+
+function showDetails() {
+    mainContentEl.classList.remove('hidden');
+}
+
+function hideDetails() {
+    mainContentEl.classList.add('hidden');
+}
+
+
 
